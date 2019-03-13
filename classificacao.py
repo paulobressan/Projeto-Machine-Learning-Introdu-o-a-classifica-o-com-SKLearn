5bcf6969855c2469137afdf4e307e1ac10b2b9e9
Paulo bressan <paulo.bressan@outlook.com>
2019-03-12 22:09:11 -0300

    Corrigindo nomenclatura de variaveis.

--- classificacao.py
+++ classificacao.py
@@ -9,39 +9,40 @@ cachorro1 = [0, 1, 1]
 cachorro2 = [1, 0, 1]
 cachorro3 = [1, 1, 1]
 
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+treino_x = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
 
 # porco = 1, cachorro = 0
 # Classes 0 e 1 que identifica porco e cachorro.
-classes = [1, 1, 1, 0, 0, 0]
+treino_y = [1, 1, 1, 0, 0, 0] # conhecido como labels / etiquetas
 
 # Treinando um modelo com o LinearSVC
 model = LinearSVC()
 # O fit vai treinar um modelo com os dados e a classes que esses dados correspode.
 # Isso é aprendizado supervisionado.
-model.fit(dados, classes)
+model.fit(treino_x, treino_y)
 
 # Identificar esses dados de acordo com o modelo treinado
 misterio1 = [1, 1, 1]
 misterio2 = [1, 1, 0]
 misterio3 = [0, 1, 1]
 
-testes = [misterio1, misterio2, misterio3]
+teste_x = [misterio1, misterio2, misterio3]
+# Teste de acertos. Os valores do array segnifica 0 = errou e 1 = acertou. Vamos testar a quantidade de acertos
+# que o modelo acertou com base nos testes.
+teste_y = [0, 1, 1] # labels
+
 
 # Prever o resultado
 # O sklearn usa como base de calculo a biblioteca cientifica numpy
 # O retorno de predict é um numpy.array
-previsoes = model.predict(testes)
+previsoes = model.predict(teste_x)
 
-# Teste de acertos. Os valores do array segnifica 0 = errou e 1 = acertou. Vamos testar a quantidade de acertos
-# que o modelo acertou com base nos testes.
-testes_classes = [0, 1, 1]
-corretos = (previsoes == testes_classes).sum()
-total = len(testes)
+corretos = (previsoes == teste_y).sum()
+total = len(teste_x)
 taxa_de_acerto = (corretos / total) * 100
-print(f'taxa de acerto:{taxa_de_acerto}%')
+print(f'taxa de acerto %.2f' % taxa_de_acerto )
 
 # O sklearn tem uma função que mede acuracia do modelo.
 # Primeiro parametros são os resultados verdadeiros e o segundo as previsões
-accuracy_score = accuracy_score(testes_classes, previsoes)
-print(f'taxa de acerto:{(accuracy_score * 100)}%')
+accuracy_score = accuracy_score(teste_y, previsoes)
+print(f'taxa de acerto %.2f' % (taxa_de_acerto * 100) )
