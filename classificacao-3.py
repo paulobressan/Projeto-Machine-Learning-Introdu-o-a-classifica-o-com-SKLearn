89869ead7932240bddd02b63a0f9cd9cdc34b2e3
paulobressan <paulo.bressan@outlook.com>
2019-04-05 16:32:12 -0300

    Testando algoritmo e visualizando resultado no matplot

--- classificacao-3.py
+++ classificacao-3.py
@@ -23,7 +23,6 @@ a_renomear = {
 
 dados = dados.rename(columns=a_renomear)
 
-
 # Para facilitar o entendimento dos dados, em vez de não finalizado, vamos trocar para finalizados e os valores invertidos.
 troca = {
     1: 0,
@@ -36,13 +35,13 @@ dados['finalizado'] = dados.nao_finalizado.map(troca)
 
 # criar um grafico de frequencia onde o x são as horas e o y os preços e os date = dados
 # podemos definir cores em cada pronto pelo hue com base no finalizado
-sns.scatterplot(x='horas_esperadas', y='preco', hue='finalizado', data=dados)
+# sns.scatterplot(x='horas_esperadas', y='preco', hue='finalizado', data=dados)
 
 # podemos usar o rel plot para plotar dois grafico com duas colunas com base nos finalizado e não finalizados
-sns.relplot(x='horas_esperadas', y='preco',
-            col='finalizado',  hue='finalizado', data=dados)
+# sns.relplot(x='horas_esperadas', y='preco',
+#             col='finalizado',  hue='finalizado', data=dados)
 
-# exibir janela com o grafico
+# # exibir janela com o grafico
 # plt.show()
 
 # separando dados para treinar um modelo para prever
@@ -78,8 +77,54 @@ acuracia = accuracy_score(teste_y, previsoes)
 print('A acuracia foi %.2f%%' % (acuracia * 100))
 
 # O algortmo de base é importante para saber se o modelo esta ruim ou bom, pelo fato de testar
-# um dados de um unico valor. 
+# um dados de um unico valor.
 # gerar um arrar com 540 posições de valor 1
 previsoes_de_base = np.ones(540)
 acuracia = accuracy_score(teste_y, previsoes_de_base)
 print('A acuracia do algoritmo de baseline foi %.2f%%' % (acuracia * 100))
+
+# Para analisar porque a acursacia do modelo esta ruim, vamos testar o modelo de acordo com o grafico gerado
+
+# sns.scatterplot(x=teste_x.horas, y='preco', hue='finalizado', data=dados)
+
+# plt.show()
+
+# temos que min e max de teste_x e y. Na posição x = horas_esperadas e y = preco
+x_min = teste_x.horas_esperadas.min()
+x_max = teste_x.horas_esperadas.max()
+y_min = teste_x.preco.min()
+y_max = teste_x.preco.max()
+
+print(x_min, x_max, y_min, y_max)
+
+# Para marcar no grafico os pontos do treinamento, vamos definir um tamanho de pixel
+pixels = 100
+# No eixo x, entre o minimo e maximo, de quanto a quantos vamos ter pontos de pixels
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixels)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixels)
+
+# Com o eixo x e eixo y pronto, vamos mesclar em um grid, porem o meshgrid somente cria 100x100 repedindo 100 vezes
+xx, yy = np.meshgrid(eixo_x, eixo_x)
+# vamos concatenar o x com o y criando pontos validos no plano cartesiano
+pontos = np.c_[xx.ravel(), yy.ravel()]
+print(pontos)
+
+# com todos os prontos prontas, podemos prever com o modelo os resultados desses pontos
+Z = modelo.predict(pontos)
+# porem a previsão gerada pelo modelo tem a dimenção (10000,) e nosso xx e yy é 100x100. Vamos redimencionar 10000 para 100x100
+Z = np.reshape(Z, (100,100))
+print(Z.shape)
+
+# Vamos desenha no grafico a "curva" de acertos e erros
+# alpha é a transparencia
+print(xx, yy)
+plt.contourf(xx, yy, Z, alpha=0.3)
+
+# Vamos plotar os resultados da previsão. O scatter vai plotar a dispersão dos pontos
+# c = cor, pore exemplo c="blue", ou uma sequencia de valores 0 ou 1.
+# s = size, tamanho dos pontos
+plt.scatter(teste_x.horas_esperadas, teste_x.preco, c=teste_y.finalizado, s=1)
+
+# exibir janela com o grafico
+plt.show()
+
