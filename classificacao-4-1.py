424b4b4391dbcf7e3efb4a4c3340d1971906b96c
paulo.bressan <paulo.bressan@luizalabs.com>
2019-04-09 14:16:00 -0300

    Treinando modelo para prever se um veiculo vai ser vendido com determinadas caracteristicas com o SVC e utlizando scaler para calcular a media, dispersão e desvio padrão para extrair treino_x e teste_x.

--- classificacao-4-1.py
+++ classificacao-4-1.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from datetime import datetime
 from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
+# O standardScale vai preprocessar os dados separando o treino_x e o teste_x de acordo com a disperção da media e dividir pelo desvio padrão
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 # Estimador de acuracia. Vamos usar para termos uma base de dados onde podemos estimar a acuracia do modelo
 from sklearn.dummy import DummyClassifier
@@ -48,39 +50,19 @@ SEED = 5
 np.random.seed(SEED)
 # Extraindo o treino e 25% dos dados em testes
 # O stratify, se a variável y for uma variável categórica binária com valores 0 e 1 e houver 25% de zeros e 75% de uns, stratify = y garantirá que sua divisão aleatória tenha 25% de 0s e 75% de 1s.
-treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, stratify=y)
+raw_treino_x, raw_teste_x, treino_y, teste_y = train_test_split(x, y, test_size=0.25, stratify=y)
+
+# O Scaler vai pegar os dados e analisar a media dos dados e suas disperção e dividir pelo desvio padrão
+scaler = StandardScaler()
+scaler.fit(raw_treino_x)
+treino_x = scaler.transform(raw_treino_x)
+teste_x = scaler.transform(raw_teste_x)
 
 print("Treinamentos com %d elementos e testaremos com %d elementos" % (len(treino_x), len(teste_x)))
 
-modelo = LinearSVC()
+modelo = SVC()
 modelo.fit(treino_x, treino_y)
 previsoes = modelo.predict(teste_x)
 
 acuracia = accuracy_score(teste_y, previsoes) * 100
 print("A acuracia for %.2f%%" % acuracia)
-
-# Criando o estimador
-# O dummy estima os valores estratificados de acordo com o treino_y. Se tiver 30% de 1 o dummy vai criar 30% de 1 e vice-versa para o 0
-
-# O Dummy utiliza o numpy random. Portanto a SEED=5 definida acima vai impactar nos dados gerado pelo dummy
-dummy_stratified = DummyClassifier()
-# O dummy vai treinar
-dummy_stratified.fit(treino_x, treino_y)
-## O dummy vai prever
-# previsoes = dummy_stratified.predict(teste_x)
-## Acuracia do dummy
-# acuracia = accuracy_score(teste_y, previsoes) * 100
-# print("A acuracia do Dummy Stratified %.2f%%" % acuracia)
-# USANDO A ACURACIA DO DUMMY
-acuracia = dummy_stratified.score(teste_x, teste_y)
-print("A acuracia do Dummy Stratified %.2f%%" % (acuracia * 100))
-
-dummy_mostfrequent = DummyClassifier(strategy="most_frequent")
-# O dummy vai treinar
-dummy_mostfrequent.fit(treino_x, treino_y)
-## O dummy vai prever
-# previsoes = dummy_mostfrequent.predict(teste_x)
-## Acuracia do dummy
-# acuracia = accuracy_score(teste_y, previsoes) * 100
-acuracia = dummy_mostfrequent.score(teste_x, teste_y)
-print("A acuracia do Dummy Mostfrequent %.2f%%" % (acuracia * 100))
